61a3475ecaa1f83437cbf1ac225a22a22b85b740
Ashweini <ashwinirk43@gmail.com>
2024-10-18 21:17:57 +0530

    pushTwo

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,7 +3,7 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("I am from Master");
-		System.out.println("Making changes in gitHub only"):
+		System.out.println("Making changes in gitHub only");
 	}
 
 }
